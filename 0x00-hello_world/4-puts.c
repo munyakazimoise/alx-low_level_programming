9a900ce7a64cc201413dce780ef0caecae58fb45
munyakazimoise <moisemunyakazi@gmail.com>
2022-12-09 01:44:10 -0800

    kki

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,14 @@
-/*
- * File: 4-puts.c
- * Auth: Brennan D Baraban <375@holbertonschool.com>
- */
-
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
+ * main - Entry point 
  *
- * Return: Always 0.
+ * Return: Always 0  (Sucess)
  */
+
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
-
         return (0);
+
 }	
