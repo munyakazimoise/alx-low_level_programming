46444ea0e60a29d67d6843529862a62a3e062845
munyakazimoise <moisemunyakazi@gmail.com>
2022-12-09 05:15:58 -0800

    jjut

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Prints a multilingual puzzle
  *
- * Return: Always 0  (Sucess)
+ * Return: Always 0
  */
+
 
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
 
-}	
+
+	return (0);
+}
